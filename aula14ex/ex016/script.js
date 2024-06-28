829a95d05c76541d4b2397f86c34fd79ade8a7c2
Filipe Almeida <filipeagt.92@gmail.com>
2024-06-28 15:16:42 -0300

    Exercícios parte 5

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,15 +1,35 @@
 function contar() {
-    var inicio = Number(document.getElementById('inicio').value)
-    var fim = Number(document.getElementById('fim').value)
-    var passo = Number(document.getElementById('passo').value)
-    var res = document.getElementById('res')
-    var foot = '&#x1F9B6'
-    var flag = '&#x1F3C1'
+    let inicio = document.getElementById('inicio').value
+    let fim = document.getElementById('fim').value
+    let passo = document.getElementById('passo').value
+    let res = document.getElementById('res')
 
-    res.innerHTML = ""
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
+        //window.alert('[ERRO!] Faltam dados!')
+        res.innerHTML = "Impossível contar!"
+    } else {
+        res.innerHTML = "Contando: <br>"
+        inicio = Number(inicio)
+        fim = Number(fim)
+        passo = Number(passo)
+        let foot = '&#x1F9B6' // HTML emoji
+        let flag = ` \u{1F3C1}` // JavaScript emoji
 
-    for (i=inicio;i<=fim;i=i+passo) {
-        res.innerHTML +=  i + foot
+        if (passo <= 0) {
+            window.alert('Passo inválido! Considerado passo 1.')
+            passo = 1
+        }
+
+        if (inicio < fim) { // Contagem crescente
+            for (let i=inicio;i<=fim;i+=passo) {
+                res.innerHTML +=  ` ${i} ${foot}`
+            }
+            
+        } else { // Contagem regressiva
+            for (let i=inicio;i>=fim;i-=passo) {
+                res.innerHTML += ` ${i} ${foot}`
+            }
+        }
+        res.innerHTML += flag
     }
-    res.innerHTML += flag
 }
