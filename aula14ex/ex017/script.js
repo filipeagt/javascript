56bf6ff28dd6fc2af7470360da380ad6757f138e
Filipe Almeida <filipeagt.92@gmail.com>
2024-06-28 17:05:01 -0300

    Exercícios parte 6

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,4 @@
+/*
 function tabuada() {
     var num = document.getElementById('num').value
     var txt = document.getElementById('txt')
@@ -8,6 +9,26 @@ function tabuada() {
         txt.innerHTML = ""
         for (var i=1;i<=10;i++) {
             txt.innerHTML += `${num} x ${i} = ${num*i}\n`
+        }
+    }
+}
+*/
+
+function tabuada() {
+    var num = document.getElementById('num').value
+    var tabuada = document.getElementById('tabuada')
+
+    if (num.length == 0) {
+        alert('Digite um número...')
+    } else {
+        num = Number(num)
+        tabuada.innerHTML = ""
+        
+        for (var i=1;i<=10;i++) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${i} = ${num*i}`
+            item.value = `tab${i}`
+            tabuada.appendChild(item)
         }
     }
 }
