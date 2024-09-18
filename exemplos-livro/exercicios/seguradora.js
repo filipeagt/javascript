bb4a286669be0ff5941b08cc95c4795cdbde8273
Filipe Almeida <filipeagt.92@gmail.com>
2024-09-18 20:51:24 -0300

    Exercício 10 - cálculo apólice

--- exemplos-livro/exercicios/seguradora.js
+++ exemplos-livro/exercicios/seguradora.js
@@ -1,6 +1,27 @@
 function calcular() {
     if (validarSexo() && validarNasc() && validarFabric() && validarValor() && validarBonus()) {
-        res.innerHTML = "OK!"
+        let apolice = 0
+        let fabricacao = parseInt(fabric.value)
+        let valor = parseFloat(val.value)
+        let sexo = f.checked ? 'F' : 'M'
+        let anoAtual = new Date().getFullYear()
+        let nascimento = parseInt(nasc.value)
+        let idade = anoAtual - nascimento
+        let porcBonus = parseInt(bonus.value)/100
+
+        if (fabricacao >= 2010) apolice = valor * (1.25/100)
+        else if (fabricacao >= 2000) apolice = valor * (1.75/100)
+        else if (fabricacao >= 1980) apolice = valor * (2/100)
+        else apolice = valor * (2.5/100)
+
+        if (sexo == 'F') apolice *= .90 // Desconto de 10%
+        else apolice *= 1.05 // Acrescimo de 5%
+
+        if (idade < 30 || idade > 60) apolice *= 1.2 // Acrescenta 20%
+
+        apolice *= (1-porcBonus) // Desconta valor informado
+
+        res.innerHTML = `Valor da apólice: R$ ${apolice.toFixed(2).replace('.', ',')}`
     } else {
         res.innerHTML = "Corrija os dados e tente novamente"
     }
@@ -8,10 +29,10 @@ function calcular() {
 
 function validarSexo() {
     if (f.checked == true || m.checked == true) {
-        sex.style.background = "white"
+        sex.style.background = "transparent"
         return true
     } else {
-        sex.style.background = "yellow"
+        sex.style.background = '#136d56'
         return false
     }
 }
@@ -28,8 +49,8 @@ function validarNasc() {
             certo = true
         }
     }
-    if (certo) nasc.style.background = "white"
-    else nasc.style.background = "yellow"
+    if (certo) nasc.style.background = "transparent"
+    else nasc.style.background = "#136d56"
     return certo
 }
 
@@ -45,8 +66,8 @@ function validarFabric() {
             certo = true
         }
     }
-    if (certo) fabric.style.background = "white"
-    else fabric.style.background = "yellow"
+    if (certo) fabric.style.background = "transparent"
+    else fabric.style.background = "#136d56"
     return certo
 }
 
@@ -62,8 +83,8 @@ function validarValor() {
             certo = true
         }
     }
-    if (certo) val.style.background = "white"
-    else val.style.background = "yellow"
+    if (certo) val.style.background = "transparent"
+    else val.style.background = "#136d56"
     return certo
 }
 
@@ -73,13 +94,13 @@ function validarBonus() {
         certo = false
     } else {
         let porcBonus = parseInt(bonus.value)
-        if (porcBonus <= 0 || porcBonus > 25) {
+        if (porcBonus < 0 || porcBonus > 25) {
             certo = false
         } else {
             certo = true
         }
     }
-    if (certo) bonus.style.background = "white"
-    else bonus.style.background = "yellow"
+    if (certo) bonus.style.background = "transparent"
+    else bonus.style.background = "#136d56"
     return certo
 }
