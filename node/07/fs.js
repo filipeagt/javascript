9f7aa4f8702e0e8f4b2453a072f5510f386fa9c0
filipeagt <filipeagt.92@gmail.com>
2025-06-06 20:26:30 -0300

    Aula 8 Manipulação de arquivos

--- node/07/fs.js
+++ node/07/fs.js
@@ -33,6 +33,7 @@ if(fs.existsSync('./public')) {
     console.log('Impossível renomear por que "public" não existe!');
 }
 */
+/*
 if(fs.existsSync('./filipe')) {
     fs.rm('./filipe',{recursive:true} , (err)=>{ //fs.rmdir(path, { recursive: true }) Depreciado
         if(err){
@@ -43,4 +44,61 @@ if(fs.existsSync('./filipe')) {
 } else {
     console.log('Impossível deletar por que "filipe" não existe!');
 }
+*/
 
+/* Criar / Atulaizar arquivos */
+
+//fs.writeFile() - substitui o conteúdo do arquivo caso exista, se nã existir ele cria um novo com o conteúdo passado
+/*
+if(!fs.existsSync('./public/teste.txt')){
+    fs.writeFile('./public/teste.txt', 'Teste de conteúdo!', (err)=>{
+        if(err){
+            throw err;
+        }
+        console.log('Arquivo criado com sucesso!');
+    })
+}
+*/
+
+//fs.appendFile() - adiciona contúdo num arquivo, se o arquivo não existri ele cria
+/*
+fs.appendFile('outroarquivo.txt', '\nAdicionado conteúdo!', (err)=>{
+    if(err) {
+        throw err;
+    }
+    console.log('Arquivo Atualizado!')
+})
+*/
+//fs.open() - abre um arquivo para leitura, se o arquivo não existir ele cria vazio. Para criar um arquivo usar a flag "w" 
+/*
+fs.open('arquivo.txt', 'w', (err, file)=>{
+    if(err) {
+        throw err;
+    }
+    console.log('Salvo!')
+})
+*/
+
+/* Ler Arquivos */
+// fs.readFile()
+
+/* Renomear / Excluir */
+// fs.rename() - Renomear arquivo
+/*
+fs.rename('arquivo.txt', 'filipe.txt', (err)=>{
+    if(err) {
+        throw err;
+    }
+    console.log('Arquivo renomeado!');
+})
+*/
+
+// fs.unlink() - Excluir arquivo
+/*
+fs.unlink('filipe.txt', (err)=>{
+    if(err) {
+        throw err;
+    }
+    console.log('Arquivo deletado!');
+})
+*/
--- node/07/index.js
+++ node/07/index.js
@@ -1,6 +1,9 @@
 // Módulo de WEB Server
 const http = require('http');
 
+//Módulo de arquivos do sistema - File System
+const fs =  require('fs');
+
 const hostname = '192.168.1.8';
 const PORT =  process.env.PORT || 3000;
 
@@ -8,9 +11,15 @@ const server = http.createServer((req, res)=>{
     let url = req.url;
 
     if (url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html; charset=utf-8');
-        res.end('<h1>Olá, Mundo!<h1>');
+        fs.readFile('./public/index.html', (err, conteudo)=>{
+            if(err) {
+                throw err;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/html; charset=utf-8');
+            res.end(conteudo);
+        });
+        
     }
 
     if (url === '/sobre') {
