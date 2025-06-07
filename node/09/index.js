702a40db797624e18d4b6ad2572d5f7e4cc07ba2
filipeagt <filipeagt.92@gmail.com>
2025-06-07 09:29:52 -0300

    Aula 9 framework express

---
+++ node/09/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//config
+const PORT =  process.env.PORT || 3000
+
+//GET, POST, PUT, DELETE
+app.get('/', (req, res)=>{
+    //res.send('<h1>Hello, World!</h1>');
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
