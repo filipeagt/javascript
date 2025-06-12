aa90f74e89a444ebc61dd5faef1af43e6eebd74e
filipeagt <filipeagt.92@gmail.com>
2025-06-11 22:04:00 -0300

    Aula 10 roteamento e parâmetros

--- node/10/index.js
+++ node/10/index.js
@@ -1,5 +1,8 @@
-const express = require('express');
+const express = require('express'); //npm i express --save
 const app = express();
+
+//Importar o roteamento
+const carros = require('./routes/carros')
 
 //config
 const PORT =  process.env.PORT || 3000
@@ -7,12 +10,14 @@ const PORT =  process.env.PORT || 3000
 //GET, POST, PUT, DELETE
 app.get('/', (req, res)=>{
     //res.send('<h1>Hello, World!</h1>');
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html')
 });
 
 app.get('/sobre', (req, res)=>{
     res.send('<h1>Página Sobre</h1>');
 });
+
+app.use('/carros', carros);
 
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando em http://localhost:${PORT}`);
