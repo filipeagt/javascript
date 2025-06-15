37cf028ea1510b72eb40a5253deee7a77bc327fc
Filipe Almeida <filipeagt.92@gmail.com>
2025-06-15 20:35:17 -0300

    Aula 13 fron-end do crud

--- node/crud/index.js
+++ node/crud/index.js
@@ -10,8 +10,18 @@ app.engine('hbs', hbs.engine({
 }));
 app.set('view engine', 'hbs');
 
+app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
-    res.render('index',{nome:'Filipe'});
+    res.render('index',{NavActiveCad:true});
+});
+
+app.get('/users',(req,res)=>{
+    res.render('users',{NavActiveUsers:true});
+});
+
+app.get('/editar',(req,res)=>{
+    res.render('editar');
 });
 
 app.listen(PORT,()=>{
