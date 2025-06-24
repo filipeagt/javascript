2a3c1208fdd706c7ac816101c4942aaa06c264f9
filipeagt <filipeagt.92@gmail.com>
2025-06-24 16:13:33 -0300

    Aula 3 introdução JSX

--- react/app.js
+++ react/app.js
@@ -1,2 +1,35 @@
+/*
+//Criar um elemento com JavaScript puro
+//Imperativo
+const h1 = document.createElement("h1");
+h1.textContent = "Olá, mundo JS!";
+h1.className = "header";
+document.getElementById("root").append(h1);
+console.log(h1);
+*/
+
+//JSX
+//Declarativo
+
+function nomeCompleto(user) {
+    return user.nome + " " + user.sobrenome;
+}
+
+const nome = "Filipe";
+const usuario = {
+    nome: "Filipe",
+    sobrenome: "Almeida"
+}
+const pagina = (
+    <div>
+        <h1 className="header">Olá, {nomeCompleto(usuario)}!</h1>
+        <p>Isso é um parágrafo</p>
+    </div>
+);
+//console.log(elemento);
+
 //Básico do react
-ReactDOM.render(<h1>Olá, Mundo!</h1>, document.getElementById("root"));
+ReactDOM.render(
+    pagina,
+    document.getElementById("root")
+);
