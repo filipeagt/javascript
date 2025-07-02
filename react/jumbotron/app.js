27821fe2f1c346d9365aba11c2efff0089127e18
filipeagt <filipeagt.92@gmail.com>
2025-07-02 14:53:39 -0300

    Aula 13 listas e chaves

--- react/jumbotron/app.js
+++ react/jumbotron/app.js
@@ -1,15 +1,42 @@
-function App() {
-    return (
-        <main>
-            <div className="container py-4">
-                <Header />
-                <Contador btnNome={false} />
-                <CustomJumbotron />
-                <DoisJumbotrons />
-                <Footer />
-            </div>
-        </main>
+function App(props) {
+    const usuarios = [
+        {
+            id: 1,
+            nome: "Filipe",
+            idade: 33
+        },
+        {
+            id: 2,
+            nome: "Taís",
+            idade: 32
+        },
+        {
+            id: 3,
+            nome: "Theo",
+            idade: 5
+        },
+        {
+            id: 4,
+            nome: "Rajado",
+            idade: 6
+        }
+    ];
+    return(
+        <ul><Item lista={usuarios} /></ul>
     );
+}
+
+function Item(props) {
+    const usuarios = props.lista;
+    
+    return (usuarios.map(
+        (usuario)=> 
+        <li key={usuario.id.toString()}>
+            Nome: {usuario.nome} <br />
+            Idade: {usuario.idade}  <br /><br />
+        </li>
+    ));
+    
 }
 
 ReactDOM.render(
