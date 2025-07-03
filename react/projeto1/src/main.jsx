5dd06dc989f184eb20ffc7e9f040bef869a45c19
filipeagt <filipeagt.92@gmail.com>
2025-07-03 13:33:57 -0300

    Aula 17 import export

--- react/projeto1/src/main.jsx
+++ react/projeto1/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Navbar, SubNavbar } from './components/navbar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Navbar />
+    <SubNavbar />
   </StrictMode>,
 )
