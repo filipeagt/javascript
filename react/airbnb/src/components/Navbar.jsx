ad7453283bbfc6287fe279e841420a8264a53b25
Filipe Almeida <filipeagt.92@gmail.com>
2025-07-06 11:51:35 -0300

    Aula 21 inicio projeto final e navbar

--- react/airbnb/src/components/Navbar.jsx
+++ react/airbnb/src/components/Navbar.jsx
@@ -7,21 +7,27 @@ export default function Navbar() {
         <div>
             <nav className="nav-topo">
                 <div className="container-airbnb">
-                    <div className="d-flex align-items-center border border-info col-sm-6">
+                    <div className="d-flex align-items-center col-sm-6">
                         <img src={logo} alt="Logo do site" className="logo" />
                     </div>
-                    <div className="d-flex align-items-center justify-content-end border border-info col-sm-6">
+                    <div className="d-flex align-items-center justify-content-end col-sm-6">
                         <a href="#" className='link-especial'>Seja um anfitrião</a>
-                        <a href="#" className='icon-nav'><i className='mdi mdi-web'></i></a>
+                        <a href="#" className='icon-nav mx-2'><i className='mdi mdi-web'></i></a>
                         <div className="dropdown">
-                            <a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
+                            <a className="button-login dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i className="fs-5 mdi mdi-menu ps-2 me-1"></i>
+                                <i className="fs-2 position-relative mdi mdi-account-circle pe-2">
+                                    <span class="position-absolute top-0 start-50 badge border border-light rounded-circle bg-danger p-2"><span class="visually-hidden">unread messages</span></span>
+                                </i>
                             </a>
 
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            <ul style={{borderRadius:'15px'}} className="dropdown-menu mt-2 py-3 border-0 shadow">
+                                <li><a className="fw-bold dropdown-item p-2 px-3" href="#">Cadastre-se</a></li>
+                                <li><a className="dropdown-item p-2 px-3" href="#">Entrar</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item p-2 px-3" href="#">Hospede em sua acomodação</a></li>
+                                <li><a className="dropdown-item p-2 px-3" href="#">Hospede uma esperiência</a></li>
+                                <li><a className="dropdown-item p-2 px-3" href="#">Ajuda</a></li>
                             </ul>
                         </div>
                     </div>
