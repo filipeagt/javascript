984a9be5615e6029f4a6e6b1fa2e1e91315b7df2
Filipe Almeida <filipeagt.92@gmail.com>
2025-07-09 10:13:01 -0300

    Aula 23 - proj final, slider comp Filtro

--- react/airbnb/src/components/PriceSlider.jsx
+++ react/airbnb/src/components/PriceSlider.jsx
@@ -12,7 +12,25 @@ export default function PriceSlider(props) {
     function changeRangeMin(e) {
         setMin(parseInt(e.target.value));
         //console.log(e.target.value);
+        var totalBarra = props.max - props.min;
+        var percentLeft = parseInt((min-props.min)/(totalBarra)*100);
+        setLeft(`${percentLeft}%`);
     }
+
+    function changeRangeMax(e) {
+        setMax(parseInt(e.target.value));
+        var totalBarra = props.max - props.min;
+        var percentRight = parseInt(((props.max-max)/(totalBarra)*100));
+        setRight(`${percentRight}%`);
+    }
+
+    useEffect(()=>{
+        document.getElementById('inputMin').value = min;
+    }, [min]);
+
+    useEffect(()=>{
+        document.getElementById('inputMax').value = max==props.max ? `${max}+` : max;
+    }, [max]);
 
     return (
         <div>
@@ -20,22 +38,22 @@ export default function PriceSlider(props) {
                 <div style={{left: left, right: right}} className="progress"></div>
             </div>
             <div className="range-input">
-                <input onClick={changeRangeMin} type="range" id='range-min' value={min} min={props.min} max={props.max} step={props.step} />
-                <input type="range" id='range-max' value={max} min={props.min} max={props.max} step={props.step} />
+                <input onChange={changeRangeMin} type="range" id='range-min' value={min} min={props.min} max={props.max} step={props.step} />
+                <input onChange={changeRangeMax} type="range" id='range-max' value={max} min={props.min} max={props.max} step={props.step} />
             </div>
             <div className="row mt-4">
                 <div className="col">
                     <label htmlFor="inputMin" className="text-muted">preço mínimo</label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">R$</span>
-                        <input id='inputMin' type="text" class="form-control" placeholder="Min" />
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">R$</span>
+                        <input id='inputMin' type="text" className="form-control" placeholder="Min" />
                     </div>
                 </div>
                 <div className="col">
                     <label htmlFor="inputMax" className="text-muted">preço máximo</label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">R$</span>
-                        <input id='inputMax' type="text" class="form-control" placeholder="Max" />
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">R$</span>
+                        <input id='inputMax' type="text" className="form-control" placeholder="Max" />
                     </div>
                 </div>
             </div>
