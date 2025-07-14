067d095b5ea5c170bb4d42411b94a25a7b5262f9
filipeagt <filipeagt.92@gmail.com>
2025-07-14 20:53:26 -0300

    Aula 27 - 13min

--- react/airbnb/src/App.jsx
+++ react/airbnb/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 //Importar Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
@@ -15,6 +16,30 @@ import ModalFilter from './components/ModalFilter';
 import Card from './components/Card';
 
 function App() {
+  //Estados da Aplicação
+  const [isLoading, setIsLoading] = useState(true);
+  const [catId, setCatId] = useState(1);
+  const [allHouses, setAllHouses] = useState([]);
+  const [filterHouses, setFilterHouses] = useState([]);
+
+  //Primeira vez que a aplicação rodou
+  useEffect(() => {
+    fetch('https://curso.programacaoweb.com.br/api-airbnb/')
+      .then((resposta) => {
+        if (resposta.ok) {
+          return resposta.json();
+        }
+        throw new Error('Algo deu errado!');
+      }).then((respostaJSON) => {
+        setAllHouses(respostaJSON);
+      }).catch((erro) => {
+        console.log(erro);
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log(allHouses);
+  }, [allHouses])
 
   return (
     <div>
